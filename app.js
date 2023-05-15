febd812639ce7c1037a839f4835237b8a2abb924
hassanShakur <hass.shakurhassan@gmail.com>
2023-05-15 18:18:41 +0300

    Fixed linter error and removed unnecesary logs

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-console.log('hello');
 const menuIcon = document.querySelector('.menu-icon');
 const closeMenuIcon = document.querySelector('.close-menu');
 const menu = document.querySelector('.menu');
+
 menuIcon.addEventListener('click', () => {
   menu.classList.add('show-menu');
   document.body.classList.add('menu-open');
 });
+
 closeMenuIcon.addEventListener('click', () => {
   menu.classList.remove('show-menu');
   document.body.classList.remove('menu-open');
